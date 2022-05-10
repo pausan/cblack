b0a17331321c9a77402e81a9fbf60981aae4a82a
Buck Evan <buck.2019@gmail.com>
2022-05-10 19:30:04 +0000

    double-check python version during test
    this prevents errors when testing interactively

--- test.sh
+++ test.sh
@@ -13,6 +13,18 @@ err_report() {
 
 trap 'err_report' ERR
 
+PYTHON_VERSION2="$(python3 -c 'if True:
+    import sys
+    major, minor = sys.version_info[:2]
+    print(major, minor, sep=".")
+')"
+
+if [[ $PYTHON_VERSION != $PYTHON_VERSION2 ]]; then
+  echo >&2 \
+    "wrong python version! (found $PYTHON_VERSION2 expected $PYTHON_VERSION)" \
+  exit 1
+fi
+
 cat "test/${PYTHON_VERSION}_unformatted.py" | python3 cblack.py -q - > test/${PYTHON_VERSION}_reformatted.py
 
 diff -u --color=auto test/${PYTHON_VERSION}_expected.py test/${PYTHON_VERSION}_reformatted.py
